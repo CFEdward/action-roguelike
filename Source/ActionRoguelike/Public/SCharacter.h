2e79d2ad42bf8d50ba15881f0f5db9824159ffe6
Eduard Ciofu <cfedward7@gmail.com>
2022-09-29 02:32:00 +0200

    basic input and magic projectile

---
+++ Source/ActionRoguelike/Public/SCharacter.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "SCharacter.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS()
+class ACTIONROGUELIKE_API ASCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ASCharacter();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void MoveForward(const float Value);
+
+	void MoveRight(const float Value);
+
+	void PrimaryAttack();
+	
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArmComp;
+
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* CameraComp;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AActor> ProjectileClass;
+
+public:
+
+	
+
+};
